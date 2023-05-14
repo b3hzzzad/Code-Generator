cfad16ba4b99fe80c9ec0f1e831f20404d20d5f6
Behzad <ihes@yahoo.com>
2023-05-14 13:25:41 -0700

    added more class like generating random characters and added barcode generator

---
+++ java/th/o/th/qrCode.java
@@ -0,0 +1,14 @@
+package th.o.th;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class qrCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_code);
+    }
+}
